d7503876ba10f33825d7af9c207c8ee77cf2378f
regulus33 <zacharyrowden89@gmail.com>
2023-03-04 15:07:24 +0100

    Make osc visible exit
    exit
    '
    /

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,6 +18,7 @@ TapSynthAudioProcessorEditor::TapSynthAudioProcessorEditor (TapSynthAudioProcess
     : AudioProcessorEditor (&p), audioProcessor (p), osc (audioProcessor.apvts, "OSC1WAVETYPE"), adsr (audioProcessor.apvts)
 {
     setSize (400, 300);
+    addAndMakeVisible (osc);
     addAndMakeVisible (adsr);
 }
 
@@ -33,6 +34,8 @@ void TapSynthAudioProcessorEditor::paint (juce::Graphics& g)
 
 void TapSynthAudioProcessorEditor::resized()
 {
+    osc.setBounds(10,10,100,30);
     adsr.setBounds (getWidth() / 2, 0, getWidth() / 2, getHeight());
+
 }
 
